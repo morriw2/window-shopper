732b1600f5721b9a81beccf36ad80bb327db9cf4
Billy Morris <Rivers@Billys-MacBook-Pro.local>
2017-08-30 22:11:05 -0700

    added currency label using cgrect

--- window-shopper/Views/CurrencyField.swift
+++ window-shopper/Views/CurrencyField.swift
@@ -11,6 +11,21 @@ import UIKit
 @IBDesignable
 class CurrencyField: UITextField {
     
+    override func draw(_ rect: CGRect) {
+        let size: CGFloat = 20
+        let currencyLabel = UILabel(frame: CGRect(x: 5, y: (frame.size.height / 2) - size / 2, width: size, height: size))
+        currencyLabel.backgroundColor = #colorLiteral(red: 0.7697074828, green: 0.7697074828, blue: 0.7697074828, alpha: 0.8)
+        currencyLabel.textAlignment = .center
+        currencyLabel.textColor = #colorLiteral(red: 0.2549019754, green: 0.2745098174, blue: 0.3019607961, alpha: 1)
+        currencyLabel.layer.cornerRadius = 5.0
+        currencyLabel.clipsToBounds = true
+        let formatter = NumberFormatter()
+        formatter.numberStyle = .currency
+        formatter.locale = .current
+        currencyLabel.text = formatter.currencySymbol
+        addSubview(currencyLabel)
+    }
+    
     override func prepareForInterfaceBuilder() {
         customizedView()
     }
@@ -25,6 +40,7 @@ class CurrencyField: UITextField {
         backgroundColor = #colorLiteral(red: 1, green: 1, blue: 1, alpha: 0.25)
         layer.cornerRadius = 5.0
         textAlignment = .center
+        clipsToBounds = true
         
         if let p = placeholder {
             let place = NSAttributedString(string: p, attributes: [.foregroundColor:UIColor.white])
