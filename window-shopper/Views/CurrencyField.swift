14c8ecbc6a75242ca57e12fc892c4591a4fdb862
Billy Morris <Rivers@Billys-MacBook-Pro.local>
2017-08-30 20:06:35 -0700

    UI and custom textfield

---
+++ window-shopper/Views/CurrencyField.swift
@@ -0,0 +1,25 @@
+//
+//  CurrencyField.swift
+//  window-shopper
+//
+//  Created by Billy Morris on 8/30/17.
+//  Copyright © 2017 Billy Morris. All rights reserved.
+//
+
+import UIKit
+
+class CurrencyField: UITextField {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        backgroundColor = #colorLiteral(red: 1, green: 1, blue: 1, alpha: 0.25)
+        layer.cornerRadius = 5.0
+        textAlignment = .center
+        
+        if let p = placeholder {
+            let place = NSAttributedString(string: p, attributes: [.foregroundColor:UIColor.white])
+            attributedPlaceholder = place
+            textColor = #colorLiteral(red: 1.0, green: 1.0, blue: 1.0, alpha: 1.0)
+        }
+    }
+}
